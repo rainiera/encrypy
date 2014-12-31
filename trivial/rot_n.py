11dd66af28087fed7d9f0d38f40462e1a8ea1475
Rainier Ababao <rainier.ababao@gmail.com>
2014-12-30 21:22:44 -0600

    add ROT-N

---
+++ trivial/rot_n.py
@@ -0,0 +1,48 @@
+'''
+Prompts the user for plaintext, how much they want to rotate the alphabet by,
+and prints out the ciphertext.
+
+Also note:
+ord(A,Z) = 65, 90
+ord(a,z) = 97, 122
+
+as these are the values used for the normalize() function.
+'''
+
+import string
+
+def normalize(letter, minimum, maximum):
+    if letter > maximum:
+        letter -= 26
+    elif letter < minimum:
+        letter += 26
+    return letter
+
+def rotateLetter(letter, n):
+    if letter.isupper():
+        start = normalize(ord(letter) + n, 65, 90)
+    else:
+        start = normalize(ord(letter) + n, 97, 122)
+
+    newLetter = chr(start)
+    return newLetter
+
+def rotateWord(word, n):
+    result = ''
+    for letter in word:
+        result += rotateLetter(letter, n)
+    return result
+
+def getPlaintext():
+    plaintext = raw_input("\nEnter plaintext: ")
+    return plaintext
+
+def getN():
+    n = int(raw_input("Rotate by how much?: "))
+    return n
+
+if __name__ == '__main__':
+    plaintext = getPlaintext()
+    shift = getN()
+    print "\nHere is the ciphertext: "
+    print rotateWord(plaintext, shift) + "\n"
